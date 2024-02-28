c2360eb87211e3686713d277f1f5e2ef15e02710
huynhviethung214 <huynhviethung88202@gmail.com>
2024-02-28 22:19:21 +0700

    temporary changes

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,9 +1,26 @@
+const ApiError = require("../api-error");
+const { ContactService } = require("../services/contact.service");
+const MongoDB = require("../utils/mongodb.utils");
+
 exports.create = (req, res) => {
     res.send({ message: "create handler" })
 };
 
 exports.findAll = (req, res) => {
-    res.send({ message: "findAll handler" })
+    let documents = [];
+
+    try {
+        const contactService = new ContactService(MongoDB.client);
+        const { name } = req.query;
+
+        if (name) {
+            documents = await contactService.findByName(name);
+        } else {
+            documents = await contactService.find({});
+        }
+    } catch (e) {
+        return next
+    }
 };
 
 exports.findOne = (req, res) => {
@@ -25,3 +42,17 @@ exports.deleteAll = (req, res) => {
 exports.findAllFavorite = (req, res) => {
     res.send({ message: "findAllFavorite handler" })
 };
+
+exports.create = async (req, res, next) => {
+    if (!req.body?.name) {
+        return next(new ApiError(400, "Name cannot be empty"));
+    }
+
+    try {
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.create(req.body);
+        return res.send(document);
+    } catch(e) {
+        return next(new ApiError(500, "An error occured while creating the contact"));
+    }
+}
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const app = require('./app');
 const config = require('./app/config');
+const mongodb = require('./app/utils/mongodb.utils');
 
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`)
-});
+async function startServer() {
+    try {
+        await mongodb.connect(config.db.uri);
+        console.log('Database Connected');
+
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server running on port: ${PORT}`);
+        });
+    } catch (e) {
+        console.log('Cannot connect to the database!', error);
+        process.exit();
+    }
+}
